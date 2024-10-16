d64f0c894b53d70c214fc6e2d6c2e37cad201739
rohit-pokharel <rohitpokharel112@gmail.com>
2024-10-17 05:02:20 +0545

    Solution:pset0/playback

---
+++ pset0/playback/playback.py
@@ -0,0 +1,10 @@
+# Prompts the user for input and replaces each space with three periods.
+
+# Prompt the user for input
+user_input = input("Enter text: ")
+
+# Replace each space with three periods
+playback_output = user_input.replace(" ", "...")
+
+# Output the modified result
+print(playback_output)
