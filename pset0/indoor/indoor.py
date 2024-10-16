3e73024a225dca41c549ee361f4563b8c391ab62
rohit-pokharel <rohitpokharel112@gmail.com>
2024-10-17 04:57:18 +0545

    Solution:pset0/indoor

---
+++ pset0/indoor/indoor.py
@@ -0,0 +1,10 @@
+# Prompts the user for input and outputs it in lowercase.
+
+# Prompt the user for input
+user_input = input("Enter text: ")
+
+# Convert the input to lowercase
+lowercase_output = user_input.lower()
+
+# Output the result
+print(lowercase_output)
